c485c7e1d070c6bc478bf06ee34455f52a41b966
13Mai13 <maialen.berrondo@auth0.com>
2022-05-13 14:52:25 +0200

    print different vars

--- booking-app/main.go
+++ booking-app/main.go
@@ -5,8 +5,10 @@ import "fmt"
 func main (){
 	var confrenceName = "Go Conference"
 	const conferenceTickets = 50 // This values do not change
+	var remainingTickets = 50
 
 	fmt.Println(" Welcome to", confrenceName, "booking app ")
+	fmt.Println(" We have a total of", conferenceTickets, "and ", remainingTickets, "are available")
 	fmt.Println(" Get your tickets here ")
 
 	
