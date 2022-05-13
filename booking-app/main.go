f279adc2200b554b05f34a06175d135e183bac88
13Mai13 <maialen.berrondo@auth0.com>
2022-05-13 18:11:20 +0200

    new way of sting formatting

--- booking-app/main.go
+++ booking-app/main.go
@@ -7,9 +7,9 @@ func main (){
 	const conferenceTickets = 50 // This values do not change
 	var remainingTickets = 50
 
-	fmt.Println(" Welcome to", confrenceName, "booking app ")
-	fmt.Println(" We have a total of", conferenceTickets, "and ", remainingTickets, "are available")
-	fmt.Println(" Get your tickets here ")
+	fmt.Printf(" Welcome to %v booking app \n", confrenceName)
+	fmt.Printf(" We have a total of %v and %v are available \n" conferenceTickets, remainingTickets)
+	fmt.Printf(" Get your tickets here ")
 
 	
 }
