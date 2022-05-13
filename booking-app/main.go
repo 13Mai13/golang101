4517e7dd56152d4e2e57e22dc66ef56654e3ab47
13Mai13 <maialen.berrondo@auth0.com>
2022-05-13 14:49:35 +0200

    constants

--- booking-app/main.go
+++ booking-app/main.go
@@ -4,6 +4,7 @@ import "fmt"
 
 func main (){
 	var confrenceName = "Go Conference"
+	const conferenceTickets = 50 // This values do not change
 
 	fmt.Println(" Welcome to", confrenceName, "booking app ")
 	fmt.Println(" Get your tickets here ")
