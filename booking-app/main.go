7b8cbd98655acd76e538360b8cef2615353afc85
13Mai13 <maialen.berrondo@auth0.com>
2022-05-13 18:12:37 +0200

    typo solved

--- booking-app/main.go
+++ booking-app/main.go
@@ -8,7 +8,7 @@ func main (){
 	var remainingTickets = 50
 
 	fmt.Printf(" Welcome to %v booking app \n", confrenceName)
-	fmt.Printf(" We have a total of %v and %v are available \n" conferenceTickets, remainingTickets)
+	fmt.Printf(" We have a total of %v and %v are available \n", conferenceTickets, remainingTickets)
 	fmt.Printf(" Get your tickets here ")
 
 	
